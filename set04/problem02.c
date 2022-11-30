351e9c20e25aec9319b4750c4248c965c21443b5
ArushiBasal <113001830+ArushiBasal@users.noreply.github.com>
2022-11-30 09:40:13 +0000

    i learnt how to commit and push today

--- set04/problem02.c
+++ set04/problem02.c
@@ -26,10 +26,11 @@ int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
 {
   if ( result == 0)
-  printf("The points (%f%f), (%f%f) and (%f%f) don't form a triangle.\n",x1,y1,x2,y2,x3,y3);
+   printf("The points (%f%f), (%f%f) and (%f%f) form a triangle.\n",x1,y1,x2,y2,x3,y3);
   else
- printf("The points (%f%f), (%f%f) and (%f%f) form a triangle.\n",x1,y1,x2,y2,x3,y3);
+    printf("The points (%f%f), (%f%f) and (%f%f) don't form a triangle.\n",x1,y1,x2,y2,x3,y3
 }
+
 
 int main()
 {
